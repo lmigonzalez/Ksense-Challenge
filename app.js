4efd5cc76fe73ee2a0019999bac9c684046e523c
lmigonzalez <lmigonzalezs@gmail.com>
2022-04-14 19:22:29 -0400

    finish fetching post data

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const api = 'https://jsonplaceholder.typicode.com/'
+const waiting = document.getElementById('waiting')
 
 let users =[ {
 	name: 'luis',
@@ -11,17 +12,21 @@ let users =[ {
 }]
 
 const getUsers = () =>{
+
+	waiting.innerText = " Waiting for data"
+
 	fetch(`${api}users`)
 	.then(res=>{
 		return res.json()
 	})
 	.then(data=>{
-		console.log(data)
+		// console.log(data)
 		makeUsers(data)
 	})
 	.catch(err=>{
 		console.log(err)
 	})
+	.finally(() =>{ waiting.innerText = '' })
 }
 
 
@@ -29,6 +34,8 @@ const makeUsers = (data)=>{
 	
 
 	data.forEach(element => {
+	
+		const button = document.createElement('button')
 		const tr = document.createElement('tr')
 		const tdName  = document.createElement('td')
 		const tdUserName  = document.createElement('td')
@@ -36,22 +43,67 @@ const makeUsers = (data)=>{
 		const tdPhone  = document.createElement('td')
 		const tdWebsite  = document.createElement('td')
 		const table = document.getElementById('table')
+
 		tdName.innerText = element.name
 		tdUserName.innerText = element.username
 		tdEmail.innerText = element.email
 		
 		tdPhone.innerText = element.phone
 		tdWebsite.innerText = element.website
-		tr.appendChild(tdName)
+		button.appendChild(tdName)
+		tr.appendChild(button)
 		tr.appendChild(tdUserName)
 		tr.appendChild(tdEmail)
 		tr.appendChild(tdPhone)
 		tr.appendChild(tdWebsite)
 		table.appendChild(tr)
+
+		button.value = element.id
+
+		button.addEventListener('click',() =>{
+			getPost(parseInt(button.value))
+		})
 	});
 
-
-	
 } 
+
+const getPost = (value) =>{
+	const posts = document.getElementById('post-section')
+	fetch(`${api}posts`)
+	.then(res=>{
+		return res.json()
+	})
+	.then(data=>{
+		
+		let result = data.filter(element=>{
+			return element.userId === value
+		})
+		posts.innerHTML = ""
+		result.forEach(element=>{
+			const div = document.createElement('div')
+			const h3 = document.createElement('h3')
+			const p = document.createElement('p')
+			
+			h3.innerText = element.title
+			p.innerText = element.body
+			
+			div.appendChild(h3)
+			div.appendChild(p)
+			
+			posts.appendChild(div)
+			
+		})
+
+
+		console.log(result)
+	})
+	.catch(err=>{
+		console.log(err)
+	})
+	
+
+	console.log(value)
+
+}
 
 getUsers()
